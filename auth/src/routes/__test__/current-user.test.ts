90d2a33514174f32f8ff9895dedc6149ba7092fe
rudovik <giguni@ukr.net>
2022-01-05 06:26:07 +0200

    last minute change

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -9,7 +9,7 @@ it('responds with details about the current usr', async () => {
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
-    .expect(200)
+    .expect(400)
 
   expect(response.body.currentUser.email).toEqual('test@test.com')
 })
